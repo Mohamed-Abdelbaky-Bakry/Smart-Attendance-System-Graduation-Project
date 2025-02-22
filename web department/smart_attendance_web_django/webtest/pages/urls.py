214534be50f270e11ec855d4c56472f101fbe934
bakry <bakrymohamed553@gmail.com>
2025-02-22 19:44:57 +0200

    Force add smart_attendance_web_django

--- web department/smart_attendance_web_django/webtest/pages/urls.py
+++ web department/smart_attendance_web_django/webtest/pages/urls.py
@@ -5,4 +5,13 @@ urlpatterns=[
     path('',views.login,name='login'),
     path('Registration',views.Registration,name='Registration'),
     path('Forgot_Password',views.Forgot_Password,name='Forgot_Password'),
+    path('Home',views.Home,name='Home'),
+    path('Forgot_Password/Password_Reset',views.Password_Reset,name='Password_Reset'),
+    path('Home/Dashboard',views.Dashboard,name='Dashboard'),
+    path('Home/Search',views.Search,name='Search'),
+    path('Home/Search/Profile',views.Profile,name='Profile'),
+    path('Home/message',views.message,name='message'),
+    path('Home/Report',views.Report,name='Report'),
+    path('Home/Leave_Application',views.Leave_Application,name='Leave_Application'),
+    path('Home/Leave_Application/Approval',views.Approval,name='Approval'),
 ]
--- web department/smart_attendance_web_django/webtest/pages/views.py
+++ web department/smart_attendance_web_django/webtest/pages/views.py
@@ -8,3 +8,30 @@ def Registration(request):
 
 def Forgot_Password(request):
     return render(request,'pages/Forgot_Password.html')
+
+def Home(request):
+    return render(request,'pages/Home.html')
+
+def Password_Reset(request):
+    return render(request, 'pages/Password_Reset.html')
+
+def Dashboard(request):
+    return render(request, 'pages/Dashboard.html')
+
+def Search(request):
+    return render(request, 'pages/Search.html')
+
+def Profile(request):
+    return render(request, 'pages/Profile.html')
+
+def message(request):
+    return render(request, 'pages/message.html')
+
+def Report(request):
+    return render(request, 'pages/Report.html')
+
+def Leave_Application(request):
+    return render(request, 'pages/Leave_Application.html')
+
+def Approval(request):
+    return render(request, 'pages/Approval.html')
